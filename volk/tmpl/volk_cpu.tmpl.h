3bdbc33530e18eb73e8e578ff86e88a1657a01c0
Josh Blum <josh@joshknows.com>
2012-09-04 15:30:15 -0700

    volk: added and fixed LGPL copyright assignments for libvolk
    GNURadio copyright headers that were copied by me have been replaced
    with headers that are either custom to volk or non-gnuradio specific.
    
    Build related files such as CMakeLists or python helpers are licensed GPL.
    
    Any files compiled into libvolk or templates that generate files for libvolk
    now have LGPL licences with copyright assignment to FSF.

--- volk/tmpl/volk_cpu.tmpl.h
+++ volk/tmpl/volk_cpu.tmpl.h
@@ -1,23 +1,19 @@
-/*
- * Copyright 2011-2012 Free Software Foundation, Inc.
- *
- * This file is part of GNU Radio
- *
- * GNU Radio is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
- *
- * GNU Radio is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GNU Radio; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street,
- * Boston, MA 02110-1301, USA.
- */
+//
+// Copyright 2012 Free Software Foundation, Inc.
+//
+// VOLK is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VOLK is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with VOLK.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef INCLUDED_VOLK_CPU_H
 #define INCLUDED_VOLK_CPU_H
