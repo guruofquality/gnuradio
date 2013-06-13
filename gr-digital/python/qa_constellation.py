53223d7e7c60974bd56ec3287fdc184ea910087b
Josh Blum <josh@joshknows.com>
2013-06-13 01:33:28 -0700

    digital: a block should not hold a reference to itself

--- gr-digital/python/qa_constellation.py
+++ gr-digital/python/qa_constellation.py
@@ -158,7 +158,8 @@ class mod_demod(gr.hier_block2):
         # TX
         self.constellation = constellation
         self.differential = differential
-        self.blocks = [self]
+        import weakref
+        self.blocks = [weakref.proxy(self)]
         # We expect a stream of unpacked bits.
         # First step is to pack them.
         self.blocks.append(
@@ -199,7 +200,7 @@ class mod_demod(gr.hier_block2):
         # connect to block output
         check_index = len(self.blocks)
         self.blocks = self.blocks[:check_index]
-        self.blocks.append(self)
+        self.blocks.append(weakref.proxy(self))
 
         self.connect(*self.blocks)
 
